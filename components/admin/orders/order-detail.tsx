0bcba7204c87e211584724b2939c764515ea2078
Imran Khan <imranjpj12@gmail.com>
2026-04-13 19:25:45 +0500

    default note updated

--- components/admin/orders/order-detail.tsx
+++ components/admin/orders/order-detail.tsx
@@ -45,10 +45,9 @@ export function OrderDetail({ orderId }: OrderDetailProps) {
   const queryClient = useQueryClient();
   const [isPaymentDialogOpen, setIsPaymentDialogOpen] = React.useState(false);
   const [customNote, setCustomNote] = React.useState(
-    `We Do Hereby Give This Warranty That
-The Medicines Prepared By Root's Pharma Lhr.
-As Sold By Us Are Homeopathic Medicines And
-Do Not Contravene in Any Way With Any Provision of The Drap Act 2012`,
+    `We do hereby give this warranty that the medicines described in this invoice has sold by us do not contravene in any way in any provision of the DRAP ACT 2012. 
+Sales Manager Roots Pharma (Pvt) Ltd
+Note: Claim atleast 3 months before the expiry of medicine`,
   );
   const { data: order, isLoading, isError } = useGetOrderById(orderId);
   const recordPayment = useRecordPayment();
